2c572e0b8acca1329da9a4e5994deaaab284d0f2
zohairshaikh <zohair.shaikh@livehealth.in>
2018-03-29 14:59:27 +0530

    Added support for accepting StrictRedis connection

--- generator.py
+++ generator.py
@@ -1,25 +1,26 @@
 
 
-import redis
+from redis import StrictRedis
 import warnings
-
-connection = redis.StrictRedis(host='0.0.0.0', port=6379,db=7)
 
 BUFFER = 20
 
 class Generator(object):
-    def __init__(self, start_from=1, key=None):
+    def __init__(self, start_from=1, key=None, connection=None):
         if key is None:
             raise Exception('Param Key cannot be null')
+        if not isinstance(connection, StrictRedis):
+            raise Exception('connection should be an object of StrictRedis')
+        self.connection = connection
         self.key = key
-        if connection.exists(key):
-            current_id = connection.lindex(key, (connection.llen(key)-1))
+        if self.connection.exists(key):
+            current_id = self.connection.lindex(key, (connection.llen(key)-1))
             if current_id is not None:
                 if int(current_id) > start_from and start_from > 1:
                     raise Exception("Current sequence of id "+str(current_id)+" cannot be greated than start_of.")
 
             # Clearing old values as new start_from passed for the key
-            connection.delete(key)
+            self.connection.delete(key)
 
             # Regenerating buffer ids
             self.__addtolist(start_from, key)
@@ -30,10 +31,10 @@ class Generator(object):
 
     def __addtolist(self, start_from, key):
         for x in range(start_from, (start_from + BUFFER)):
-            connection.rpush(key, x)
+            self.connection.rpush(key, x)
 
 
     def getId(self):
-        unique_id = connection.lpop(self.key)
-        connection.rpush(self.key, (int(unique_id) + BUFFER))
+        unique_id = self.connection.lpop(self.key)
+        self.connection.rpush(self.key, (int(unique_id) + BUFFER))
         return unique_id
